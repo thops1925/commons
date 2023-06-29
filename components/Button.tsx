cfeab4cbb80f4830b52271ae0b2c7c8dd79122ec
ThopsD <74445064+thops1925@users.noreply.github.com>
2023-06-29 22:31:44 +0800

    save package

---
+++ components/Button.tsx
@@ -0,0 +1,9 @@
+const Button = ({ title }) => {
+  return (
+    <button type="button" className="flexCenter gap-3 px-4 py-3 ">
+      {title}
+    </button>
+  );
+};
+
+export default Button;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
 import { NavLinks } from "../constant/index";
+import Button from "/components//Button";
+
 const Navbar = () => {
+  const user = true;
   return (
-    <nav className="flex justify-between gap-4 px-8 py-5 border-b flex-between ">
-      <div className="flex-1">
+    <nav className="flex justify-between gap-4 border-b px-8 py-5 ">
+      <div className="flex flex-1 items-start justify-start">
         <Link href="/">
           <Image
             src="/thops3.png"
@@ -12,10 +15,10 @@ const Navbar = () => {
             width={116}
             height={43}
             alt="logo"
+            className="blur-none"
           />
         </Link>
-
-        <ul className="hidden xl:flex text-small gap-7">
+        <ul className="text-small hidden gap-7 xl:flex">
           {NavLinks.map((link) => (
             <Link href={link.href} key={link.key}>
               {link.text}
@@ -23,7 +26,20 @@ const Navbar = () => {
           ))}
         </ul>
       </div>
-      <div className="flex items-center justify-center gap-4">login</div>
+      <div className=" flex items-center justify-center gap-7 ">
+        {user ? (
+          <>
+            {/* <ProfileMenu session={session} /> */}
+
+            <Link href="/create-project">
+              <Button title="Share work" />
+            </Link>
+          </>
+        ) : (
+          <>log out</>
+        )}
+        <Footer />
+      </div>
     </nav>
   );
 };
