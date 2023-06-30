fdb4acf2e800f218034c07e3eab165342402b090
ThopsD <74445064+thops1925@users.noreply.github.com>
2023-06-30 23:01:25 +0800

    sa

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,32 +5,36 @@ import Link from "next/link";
 import React from "react";
 export const Footer = () => {
   return (
-    <footer className="flex justify-start items-start flex-col lg:px-20 py-6 px-5 w-full gap-20 bg-light-white">
-      <div className="flex flex-col gap-12">
+    <section className="flex justify-start items-start flex-col lg:px-20 py-6 px-5 gap-20 bg-light-white">
+      <div className="flex flex-col gap-12 w-full">
         <div className="flex justify-start items-start flex-col">
-          <Link href="/">
-            <Image
-              src="/thops3.png"
-              alt="logo"
-              width={116}
-              height={43}
-              className="blur-none"
-            />
-          </Link>
+          <Image
+            src="/thops3.png"
+            alt="logo"
+            width={116}
+            height={43}
+            className="blur-none"
+          />
           <p className="text-start text-sm font-normal mt-5 max-w-sm mx-4">
             "Unlocking the Power of Sharing: Join the Online Revolution of Community Collaboration"
           </p>
         </div>
         <div className="flex flex-wrap gap-12">
           <FooterCol title={footerLinks[0].title} links={footerLinks[0].links} />
-          <FooterCol title={footerLinks[1].title} links={footerLinks[1].links} />
-          <FooterCol title={footerLinks[2].title} links={footerLinks[2].links} />
+          <div className="flex-1 flex flex-col gap-4">
+            <FooterCol title={footerLinks[1].title} links={footerLinks[1].links} />
+            <FooterCol title={footerLinks[2].title} links={footerLinks[2].links} />
+          </div>
           <FooterCol title={footerLinks[3].title} links={footerLinks[3].links} />
-          <FooterCol title={footerLinks[4].title} links={footerLinks[4].links} />
-          <FooterCol title={footerLinks[5].title} links={footerLinks[5].links} />
+          <div className="flex-1 flex flex-col gap-4">
+            <FooterCol title={footerLinks[5].title} links={footerLinks[5].links} />
+            <FooterCol title={footerLinks[4].title} links={footerLinks[4].links} />
+
+          </div>
           <FooterCol title={footerLinks[6].title} links={footerLinks[6].links} />
+
         </div>
       </div>
-    </footer >
+    </ section>
   );
 };
