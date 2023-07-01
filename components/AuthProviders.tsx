a71d140d9ad6b23c6c79b749c35c373bdf996067
ThopsD <74445064+thops1925@users.noreply.github.com>
2023-07-02 00:34:02 +0800

    auth

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -1,5 +1,21 @@
+'use client'
+import { signIn, signOut, useSession } from 'next-auth/react';
+import { useState } from 'react';
+
+type Provider = {
+  id: string,
+  name: string,
+  type: string,
+  signIngUrl: string,
+  callbackUrl: string,
+  signInUrlParams: Record<string, string> | null,
+}
+
+
+
 const AuthProviders = () => {
-  return <div className="flex">
+  const [providers, setProviders] = useState<Provider | null>(null)
+  return <div>
     <p>logout</p>
   </div>;
 };
